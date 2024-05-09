e99f4ef0cff7ddaca5ca02c2c11b216162794f07
acontius <152691439+acontius@users.noreply.github.com>
2024-05-09 19:54:09 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,3 @@
-def count_arrangements(n, pieces):
-    memo = {}
-    return count_arrangements_recursive(n, pieces, 0, 0, 0, memo)
-
-
-def count_arrangements_recursive(n, pieces, width_sum, height_sum, mask, memo):
-    if mask == (1 << n) - 1:
-        return 1
-
-    if (mask, width_sum, height_sum) in memo:
-        return memo[(mask, width_sum, height_sum)]
-
-    total_arrangements = 0
-
     for i in range(n):
         if (mask & (1 << i)) == 0:
             new_width_sum = max(width_sum, pieces[i][0])
@@ -35,6 +21,5 @@ for _ in range(piec_num):
     piece = tuple(map(int, input().split()))
     pieces.append(piece)
 
-# حساب کردن و چاپ تعداد حالات مختلف
 result = count_arrangements(piec_num, pieces)
 print(f"You can make a Square in {result} Ways")
